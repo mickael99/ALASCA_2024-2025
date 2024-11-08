20a73e922049eb7188f9f4c529bcb3620e9af8d8
mickael99 <mickael.temim99@gmail.com>
2024-11-08 18:18:35 +0100

    Battery MIL simulation done

---
+++ src/fr/sorbonne_u/components/equipments/battery/mil/BatteryChargeLevelModel.java
@@ -0,0 +1,161 @@
+package fr.sorbonne_u.components.equipments.battery.mil;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import fr.sorbonne_u.components.equipments.battery.BatteryI;
+import fr.sorbonne_u.components.equipments.battery.mil.events.AbstractBatteryEvent;
+import fr.sorbonne_u.components.equipments.battery.mil.events.SetConsumeBatteryEvent;
+import fr.sorbonne_u.components.equipments.battery.mil.events.SetProductBatteryEvent;
+import fr.sorbonne_u.devs_simulation.hioa.annotations.ExportedVariable;
+import fr.sorbonne_u.devs_simulation.hioa.models.AtomicHIOA;
+import fr.sorbonne_u.devs_simulation.hioa.models.vars.Value;
+import fr.sorbonne_u.devs_simulation.models.annotations.ModelExternalEvents;
+import fr.sorbonne_u.devs_simulation.models.events.Event;
+import fr.sorbonne_u.devs_simulation.models.events.EventI;
+import fr.sorbonne_u.devs_simulation.models.time.Duration;
+import fr.sorbonne_u.devs_simulation.models.time.Time;
+import fr.sorbonne_u.devs_simulation.simulators.interfaces.AtomicSimulatorI;
+import fr.sorbonne_u.devs_simulation.utils.StandardLogger;
+
+@ModelExternalEvents(imported = {
+        SetProductBatteryEvent.class,
+        SetConsumeBatteryEvent.class
+})
+public class BatteryChargeLevelModel extends AtomicHIOA {
+
+	// -------------------------------------------------------------------------
+	// Constants and variables
+	// -------------------------------------------------------------------------
+	
+	private static final long serialVersionUID = 1L;
+	public static final String URI = BatteryChargeLevelModel.class.getSimpleName();
+    protected static final double STEP = 0.1;
+    
+    // Drain 15% of its total capacity in hour
+	protected static final double DISCHARGE_SPEED = 0.15;
+	
+	// Recharges 35% of its total capacity in hour
+	protected static final double CHARGE_SPEED = 0.35; 
+	
+	protected final Duration evaluationStep;
+
+    private BatteryI.STATE currentState;
+
+    @ExportedVariable(type = Double.class)
+    protected final Value<Double> currentChargeLevel = new Value<Double>(this);
+		
+		
+	// -------------------------------------------------------------------------
+	// Constructors
+	// -------------------------------------------------------------------------
+    
+    public BatteryChargeLevelModel(String uri, TimeUnit simulatedTimeUnit, AtomicSimulatorI simulationEngine) throws Exception {
+        super(uri, simulatedTimeUnit, simulationEngine);
+        evaluationStep = new Duration(STEP, getSimulatedTimeUnit());
+        this.getSimulationEngine().setLogger(new StandardLogger());
+    }
+
+    
+    // -------------------------------------------------------------------------
+ 	// Methods
+ 	// -------------------------------------------------------------------------
+    
+    public BatteryI.STATE getCurrentState() {
+    	return this.currentState;
+    }
+    
+    public void setProduction() {
+    	this.currentState = BatteryI.STATE.PRODUCT;
+    }
+    
+    public void setConsumption() {
+    	this.currentState = BatteryI.STATE.CONSUME;
+    }
+    
+    public void charge(Duration d) {
+    	double duration = d.getSimulatedDuration();
+    	
+    	double currentLevel = this.currentChargeLevel.getValue();
+    	double newLevel = Math.min(currentLevel + (CHARGE_SPEED * duration), 1.0);
+    	
+    	this.currentChargeLevel.setNewValue(newLevel, this.getCurrentStateTime());
+    }
+    
+    public void discharge(Duration d) {
+        double duration = d.getSimulatedDuration();
+        
+        double currentLevel = this.currentChargeLevel.getValue();
+        double newChargeLevel = Math.max(currentLevel - (DISCHARGE_SPEED * duration), 0.0);
+        
+        this.currentChargeLevel.setNewValue(newChargeLevel, this.getCurrentStateTime());
+    }
+
+    @Override
+    public void initialiseState(Time initialTime) {
+        super.initialiseState(initialTime);
+
+        currentState = BatteryI.STATE.CONSUME;
+        this.currentChargeLevel.initialise(1.0);
+
+        this.getSimulationEngine().toggleDebugMode();
+        logMessage("Simulations starts...\n");
+    }
+    
+    @Override
+    public ArrayList<EventI> output() {
+    	double v = this.currentChargeLevel.getValue();
+    	
+    	ArrayList<EventI> res = new ArrayList<>();
+        if(v <= 0.0) {
+            res.add(new SetProductBatteryEvent(currentChargeLevel.getTime()));
+            return res;
+        }
+        else if(v >= 1) {
+        	res.add(new SetConsumeBatteryEvent(currentChargeLevel.getTime()));
+            return res;
+        }
+        
+        return null;
+    }
+    
+	@Override
+	public Duration timeAdvance() {
+		return this.evaluationStep;
+	}
+
+	@Override
+    public void userDefinedInternalTransition(Duration elapsedTime) {
+        super.userDefinedInternalTransition(elapsedTime);
+
+        double v = this.currentChargeLevel.getValue();
+        if(v >= 0.0 && this.currentState == BatteryI.STATE.CONSUME) 
+            this.discharge(elapsedTime);
+        else if (v <= 1.0 && this.currentState == BatteryI.STATE.PRODUCT) 
+            this.charge(elapsedTime);
+     
+        this.currentChargeLevel.setNewValue(this.currentChargeLevel.getValue(), getCurrentStateTime());
+
+        // Tracing
+        String stateString = currentState == BatteryI.STATE.PRODUCT ? "charging" : "discharging";
+        logMessage("Battery is " + stateString + " | Charge level : " + currentChargeLevel.getValue() + " at " + currentChargeLevel.getTime() + "\n");
+    }
+	
+	@Override
+    public void userDefinedExternalTransition(Duration elapsedTime) {
+        ArrayList<EventI> currentEvents = this.getStoredEventAndReset();
+        assert	currentEvents != null && currentEvents.size() == 1;
+        Event currentEvent = (Event) currentEvents.get(0);
+
+        assert currentEvent instanceof AbstractBatteryEvent;
+        currentEvent.executeOn(this);
+
+        super.userDefinedExternalTransition(elapsedTime);
+    }
+	
+	@Override
+    public void endSimulation(Time endTime) {
+        logMessage("Simulation ends!\n");
+        super.endSimulation(endTime);
+    }
+}
---
+++ src/fr/sorbonne_u/components/equipments/battery/mil/BatteryCoupledModel.java
@@ -0,0 +1,39 @@
+package fr.sorbonne_u.components.equipments.battery.mil;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import fr.sorbonne_u.devs_simulation.hioa.models.vars.StaticVariableDescriptor;
+import fr.sorbonne_u.devs_simulation.hioa.models.vars.VariableSink;
+import fr.sorbonne_u.devs_simulation.hioa.models.vars.VariableSource;
+import fr.sorbonne_u.devs_simulation.models.CoupledModel;
+import fr.sorbonne_u.devs_simulation.models.events.EventI;
+import fr.sorbonne_u.devs_simulation.models.events.EventSink;
+import fr.sorbonne_u.devs_simulation.models.events.EventSource;
+import fr.sorbonne_u.devs_simulation.models.events.ReexportedEvent;
+import fr.sorbonne_u.devs_simulation.models.interfaces.ModelI;
+import fr.sorbonne_u.devs_simulation.simulators.interfaces.CoordinatorI;
+
+public class BatteryCoupledModel extends CoupledModel {
+
+	private static final long serialVersionUID = 1L;
+	public static final String	URI = BatteryCoupledModel.class.getSimpleName();
+
+	public BatteryCoupledModel(String uri, TimeUnit simulatedTimeUnit, CoordinatorI simulationEngine, ModelI[] submodels,
+								Map<Class<? extends EventI>, EventSink[]> imported, Map<Class<? extends EventI>, ReexportedEvent> reexported,
+								Map<EventSource, EventSink[]> connections) throws Exception
+	{
+		super(uri, simulatedTimeUnit, simulationEngine, submodels,
+			  imported, reexported, connections);
+	}
+	
+	public BatteryCoupledModel(String uri, TimeUnit simulatedTimeUnit, CoordinatorI simulationEngine, ModelI[] submodels,
+								Map<Class<? extends EventI>, EventSink[]> imported, Map<Class<? extends EventI>, ReexportedEvent> reexported,
+								Map<EventSource, EventSink[]> connections, Map<StaticVariableDescriptor, VariableSink[]> importedVars,
+								Map<VariableSource, StaticVariableDescriptor> reexportedVars,Map<VariableSource, VariableSink[]> bindings) throws Exception
+	{
+		super(uri, simulatedTimeUnit, simulationEngine, submodels,
+			  imported, reexported, connections,
+			  importedVars, reexportedVars, bindings);
+	}
+}
