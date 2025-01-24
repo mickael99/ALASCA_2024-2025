77832d54e4202a4481d6ab44bf0cd5dd7594352e
mickael99 <mickael.temim99@gmail.com>
2025-01-24 20:03:14 +0100

    Wind turbine step 3 ends

--- src/fr/sorbonne_u/components/equipments/windTurbine/CVM_WindTurbineUnitTest.java
+++ src/fr/sorbonne_u/components/equipments/windTurbine/CVM_WindTurbineUnitTest.java
@@ -27,13 +27,13 @@ public class CVM_WindTurbineUnitTest extends AbstractCVM {
 	public static double SIMULATION_START_TIME = 0.0;
 	public static double SIMULATION_DURATION = 10.0;
 	public static TimeUnit SIMULATION_TIME_UNIT = TimeUnit.HOURS;
-	public static double ACCELERATION_FACTOR = 360.0;
+	public static double ACCELERATION_FACTOR = 1000.0;
 
 	public static ExecutionType	CURRENT_EXECUTION_TYPE = ExecutionType.UNIT_TEST;
 	public static SimulationType CURRENT_SIMULATION_TYPE =  //SimulationType.NO_SIMULATION;
 															//SimulationType.MIL_SIMULATION;
-															SimulationType.MIL_RT_SIMULATION;
-															//SimulationType.SIL_SIMULATION;
+															//SimulationType.MIL_RT_SIMULATION;
+															SimulationType.SIL_SIMULATION;
 
 	public static String CLOCK_URI = "hem-clock";
 	public static String START_INSTANT = "2024-10-18T00:00:00.00Z";
